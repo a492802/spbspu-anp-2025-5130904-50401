fe7a373cb7321c9c1daa711b8f69d377db48f1e7
a492802 <q6523153@gmail.com>
2025-11-15 20:09:12 +0300

    check first argument is 1 or 2

--- borshch.diana/P3/main.cpp
+++ borshch.diana/P3/main.cpp
@@ -4,13 +4,20 @@
 #include <climits>
 
 int main(int argc, char* argv[]
-		if (argc != 4) {
-        	   if (argc < 4) {
-        	   	std::cerr << "not enough arguments" << std::endl;
-        	} else {
-        	    	std::cerr << "too many arguments" << std::endl;
-        	}
-        	return 1;
-		}
-	return 0;
+	if (argc != 4) {
+           if (argc < 4) {
+              std::cerr << "not enough arguments" << std::endl;
+        } else {
+           std::cerr << "too many arguments" << std::endl;
+        }
+        return 1;
+	}
+
+	char num = argv[1][0];
+	if (num != '1' || num != '2') {
+    	   std::cerr << "first parameter must be 1 or 2" << std::endl;
+    	return 1;
+	}
+
+return 0;
 }
